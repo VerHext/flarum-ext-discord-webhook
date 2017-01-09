7a5c0b2b41b5649f8546cc5d007c866615fb518b
Cl1608Ho <cl1608ho@gmail.com>
2017-01-09 17:18:55 +0100

    updated bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,6 @@
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
-    $events->subscribe(Cl1608Ho\DiscordWebhook\Controller::class);
-    $events->subscribe(Cl1608Ho\DiscordWebhook\Listener\AddClientAssets::class);
+    $events->subscribe(cl1608ho\DiscordWebhook\Controller::class);
+    $events->subscribe(cl1608ho\DiscordWebhook\Listener\AddClientAssets::class);
 };
